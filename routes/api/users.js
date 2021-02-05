61be57525d22fbb7aff337730da0b837c28e8e53
herman <hermanpineda94@gmail.com>
2021-02-04 20:49:49 -0600

    create user registration endpoint with jwt authentication

---
+++ routes/api/users.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router  = express.Router();
+const bcrypt  = require("bcryptjs");
+const jwt     = require("jsonwebtoken");
+const conf    = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required.").not().isEmpty(),
+    check("email", "Please provide a valid email.").isEmail(),
+    check("password", "Password must be 8 or more characters.").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure request body to extract required fields.
+    const { name, email, password } = req.body;
+    try {
+      // Check if user already exists.
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists." }] });
+      }
+
+      // Create instance of a user--not yet saved to database.
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Encrypt password.
+      const salt = await bcrypt.genSalt(10); // used to do the hashing
+      user.password = await bcrypt.hash(password, salt);
+
+      // Store new user to the database.
+      await user.save();
+
+      // Return jsonwebtoken so user is logged in after registration.
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign the payload into the JSON web token and return it to client.
+      jwt.sign(
+        payload,
+        conf.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          console.log(`User created: ${name}!`)
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error.");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express     = require("express");
-const connectToDB = require("./config/database");
+const express     = require('express');
+const connectToDB = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to database.
 connectToDB();
+
+// Initialize middleware for the body parser.
+app.use(express.json({ extended: false }));
+
+// Define routes.
+app.get('/', (req, res) => { res.send('API running...') });
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
