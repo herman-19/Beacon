2e2e9f7e71a205460190c1805579de17756a905a
herman <hermanpineda94@gmail.com>
2021-02-08 23:40:22 -0600

    initial effort for front-end

---
+++ client/beacon-client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-box">
+          <h1>Beacon</h1>
+          <p>Help your friends and family</p>
+          <p>—and let them help you!</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
